9d74d15693d9c1b78e30170aae079721e5d4ee3b
Denis Souza <denis.souza@ulteriorti.com.br>
2022-07-03 23:41:23 -0300

    merge

---
+++ src/br/com/ulteriorti/PrimeiroPrograma.java
@@ -0,0 +1,20 @@
+package br.com.ulteriorti;
+
+import br.com.ulteriorti.model.Gato;
+
+/**
+ * @author Denis Souza
+ *
+ */
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato("","",0);
+		
+		System.out.println("teste");
+		System.out.println(gato);
+
+	}
+
+}
+
